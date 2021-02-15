75b72baf96b78bcff7d10b8c10c3d49efb5fe6f3
Kieran Hanahoe <kjhanahoe@hotmail.co.uk>
2021-02-15 15:10:19 +0000

    kh: email working, still need to do validation

--- app/controllers/ContactController.scala
+++ app/controllers/ContactController.scala
@@ -1,24 +1,51 @@
 package controllers
 
 import models.{Email, EmailForm}
+import play.api.i18n.I18nSupport
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
+import courier._
+import Defaults._
+
+import javax.mail.internet.InternetAddress
+import scala.util.{Failure, Success}
 
 @Singleton
-class ContactController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ContactController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport{
 
-	val EMAIL_ADDRESS = "kjhanahoe@hotmail.co.uk"
+	val receiveEmail = "kjhanahoe@hotmail.co.uk"
 
 	def contact = Action { implicit request =>
-		EmailForm.emailForm.bindFromRequest.fold({ _ =>
-			Redirect("/")
+		Ok(views.html.contact(EmailForm.emailForm))
+	}
+
+	def sendEmail = Action { implicit request =>
+		EmailForm.emailForm.bindFromRequest.fold({ formWithErrors =>
+			BadRequest(views.html.contact(formWithErrors))
 		}, {widget =>
-			sendEmail(widget)
+			send(widget)
 			Redirect("/")
 		})
 	}
 
-	def sendEmail(email: Email): Unit = {
+	def send(email: Email) = {
+		// To send email with gmail, if you have two-factor authentication enabled, you need to set up an app password in
+		// your account settings, as courier doesn't support 2fa.
+		val mailer = Mailer("smtp.gmail.com", 587)
+			.auth(true)
+			.as("kjhanahoe@googlemail.com", "laabafjjscvatfhz")
+			.startTls(true)()
+
+		mailer(Envelope
+			.from(new InternetAddress(email.getSender))
+			.to(new InternetAddress(receiveEmail))
+			.replyTo(new InternetAddress(email.getSender))
+			.subject(email.getSubject())
+			.content(Text(email.getBody())))
+			.onComplete {
+				case Success(value) => println("message delivered")
+				case Failure(exception) => exception.printStackTrace()
+			}
 	}
 }
--- app/models/Email.scala
+++ app/models/Email.scala
@@ -2,6 +2,8 @@ package models
 
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.data.Forms.text.verifying
+import play.api.data.validation.Constraints.pattern
 
 case class Email(sender: String, subject: String, body: String) {
 	def getSender() = sender
@@ -13,8 +15,8 @@ case class EmailForm(sender: String, subject: String, body: String)
 object EmailForm {
 	val emailForm = Form(
 		mapping(
-			"sender" -> nonEmptyText,
-			"subject" -> text,
+			"sender" -> nonEmptyText.verifying(pattern(""".+@.+\..+""".r, error = "Invalid email address")),
+			"subject" -> nonEmptyText,
 			"body" -> nonEmptyText
 		) (Email.apply)(Email.unapply)
 	)
