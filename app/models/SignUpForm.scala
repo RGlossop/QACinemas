ebfe5ee9ae5a16b469797166432c8b9f3aa4ecef
Kieran Hanahoe <kjhanahoe@hotmail.co.uk>
2021-02-18 12:56:21 +0000

    kh: added password length check

--- app/models/SignUpForm.scala
+++ app/models/SignUpForm.scala
@@ -7,24 +7,24 @@ import play.api.data.validation.Constraints.pattern
 import java.time.ZoneId
 import java.util.Date
 
-case class SignUpForm(user_id: Long = 0L, first_name: String, last_name: String, date_of_birth: Date, username: String, email: String, password: String) {
+case class SignUpForm(first_name: String, last_name: String, date_of_birth: Date, username: String, email: String, password: String) {
 
 }
 
 object SignUpForm {
-	val signUpForm = Form(
+	val signUpForm: Form[User] = Form(
 		mapping(
 			"first_name" -> nonEmptyText,
 			"last_name" -> nonEmptyText,
 			"date_of_birth" -> date,
 			"username" -> nonEmptyText,
 			"email" -> text.verifying(pattern(""".+@.+\..+""".r, error = "Invalid email address")),
-			"password" -> nonEmptyText
+			"password" -> nonEmptyText(minLength = 6, maxLength = 64)
 		) (formToUser)(userToForm)
 	)
 
 	def formToUser(fName: String, lName: String, dateOfBirth: Date, username: String, email: String, password: String): User = {
-		val dob = dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+		val dob = dateOfBirth.toInstant.atZone(ZoneId.systemDefault()).toLocalDate
 		val id = 0L
 		User(id, fName, lName, dob, username, email, password)
 	}
