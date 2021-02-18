974e54680097cabfd65e0abe9784497229be93c7
RGlossop <RGlossop@qa.com>
2021-02-18 12:26:56 +0000

    QCTA-34 added discussion board with profanity filter

--- app/controllers/Startup.scala
+++ app/controllers/Startup.scala
@@ -1,20 +1,34 @@
 package controllers
 
 import com.google.inject.AbstractModule
-import models.Films
+import dao.CommentDAO
+import dao.CommentDAO.createComment
+import models.{Comment, Comments, Films}
 import slick.jdbc.MySQLProfile.api._
 
+import scala.language.postfixOps
 import javax.inject.Singleton
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
 
 
 @Singleton
 class Startup extends AbstractModule {
 
-  val DB = Database.forConfig("mySqlDB")
+  implicit val DB = Database.forConfig("mySqlDB")
   val filmTable = TableQuery[Films]
   val initSchema = DBIO.seq(filmTable.schema.createIfNotExists)
 
+  val commentTable = TableQuery[Comments]
+  val commentsDrop = DBIO.seq(commentTable.schema.dropIfExists)
+  val commentsInit = DBIO.seq(commentTable.schema.createIfNotExists)
 
+  Await.ready(DB.run(commentsDrop), 5000 millis)
+  Await.ready(DB.run(commentsInit), 5000 millis)
+
+  CommentDAO.createComment(new Comment(0, "Dave", "my Message"))
+  CommentDAO.createComment(new Comment(0, "Dave", "my Message"))
+  CommentDAO.createComment(new Comment(0, "Dave", "my Message"))
 
   DB.run(initSchema)
   println("startup")
