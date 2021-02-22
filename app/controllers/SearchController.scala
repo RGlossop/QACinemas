b59c5f458a0bd0c694e127f4d38e4cd1f76ce37c
Kieran Hanahoe <kjhanahoe@hotmail.co.uk>
2021-02-22 12:51:51 +0000

    QCTA-43 KH Search improved, still need to add search bar

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -35,8 +35,8 @@ class SearchController @Inject()(cc: ControllerComponents) extends AbstractContr
 
 	// Takes a sequence of films and returns a map of a film ID mapped to an set of all words
 	// that the film's database entry contains, stripping any words of two letters or less
-	def extractSearchTerms(films: Seq[Film]): iMap[Long, (String, Set[String])] = {
-		val result = mMap.empty[Long, (String, Set[String])]
+	def extractSearchTerms(films: Seq[Film]): iMap[Long, (String, Array[String])] = {
+		val result = mMap.empty[Long, (String, Array[String])]
 		for (film <- films) {
 			val id = film.film_id
 			val title = film.film_name
@@ -47,34 +47,62 @@ class SearchController @Inject()(cc: ControllerComponents) extends AbstractContr
 		result.toMap
 	}
 
+
 	// Takes the map of film ids to words, and a search term, and returns an array of film ids and titles
-	// ordered by the number of times the words in the search term appear in the words array
-	def sortBySearchTerms(map: iMap[Long, (String, Set[String])], searchTerm: String): Array[(Long, String)] = {
-		val searchTerms = splitWords(searchTerm)
-		val resultMap = ArrayBuffer.empty[(Long, String, Int)]
+	// ordered by the number of matches
+	def sortBySearchTerms(map: iMap[Long, (String, Array[String])], searchTerm: String): Array[(Long, String)] = {
+		val searchTerms = splitWords(searchTerm).toSet
+		val resultMap = ArrayBuffer.empty[(Long, String, Int, Int)]
 		for(film <- map){
-			var n = 0
-			for(term <- searchTerms){
-				if(film._2._2.contains(term)) {
-					n += 1
-				}
-			}
-			resultMap += ((film._1, film._2._1, n))
+			val termsMatched = checkNumberOfTermsMatching(film, searchTerms)
+			val totalMatches = checkTotalNumberOfMatches(film, searchTerms)
+			resultMap += ((film._1, film._2._1, termsMatched, totalMatches))
 		}
-		var result = ArrayBuffer.empty[(Long, String)]
-		for(entry <- resultMap.sortBy(entry => entry._3)){
-			if(entry._3 != 0) {
-				result += ((entry._1, entry._2))
-			}
-		}
-	result.toArray
+	val result = orderByMatches(resultMap)
+	result
 	}
 
 	// Takes a string and returns an array of strings, splitting at any non-word character, and transforming to lower case
-	def splitWords(string: String): Set[String] = {
-		val mixedCaseResult = string.replaceAll("\\W", " ").split(" ").toSet
+	def splitWords(string: String): Array[String] = {
+		val mixedCaseResult = string.replaceAll("\\W", " ").split(" ").toArray
 		val result = mixedCaseResult.map(word => word.toLowerCase)
 		result
+	}
+
+	// Checks the number of unique search terms matched by a film
+	def checkNumberOfTermsMatching(film: (Long, (String, Array[String])), searchTerms: Set[String] ): Int = {
+		var n = 0
+		for(term <- searchTerms){
+			if(film._2._2.toSet.contains(term)) n += 1
+		}
+		n
+	}
+
+	// Checks the total number of matches of all search terms for a film
+	def checkTotalNumberOfMatches(film: (Long, (String, Array[String])), searchTerms: Set[String]): Int = {
+		var n = 0
+		for(term <- searchTerms){
+			for(word <- film._2._2){
+				if(word.equals(term)) n += 1
+			}
+		}
+		n
+	}
+
+	// Sorts matches first by number of unique matches, then by total number of matches and returns an
+	// array of film ids and titles
+	def orderByMatches(resultMap: ArrayBuffer[(Long, String, Int, Int)]): Array[(Long, String)] = {
+		val result = ArrayBuffer.empty[(Long, String)]
+		val resultMapNoZero = resultMap.filter(entry => entry._3 != 0)
+		var i = resultMapNoZero.head._3
+		while(i > 0){
+			val resultForGivenMatches = resultMapNoZero.filter(entry => entry._3 == i).sortBy(entry => entry._4)
+			for(entry <- resultForGivenMatches){
+				result += ((entry._1, entry._2))
+			}
+			i -= 1
+		}
+		result.toArray.reverse
 	}
 
 }
