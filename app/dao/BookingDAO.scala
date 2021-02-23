061264744b526d32ffff674e195ce17b350329ab
Luk14 <lukaszplayz121@gmail.com>
2021-02-23 16:31:40 +0000

    Removed unused Classes, Changed DAO Object to Class, Added Some Controller Tests

--- app/dao/BookingDAO.scala
+++ app/dao/BookingDAO.scala
@@ -6,7 +6,7 @@ import slick.jdbc.MySQLProfile.api._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-object BookingDAO {
+class BookingDAO {
 
   lazy val DB = Database.forConfig("mySqlDB")
   lazy val bookingTable = TableQuery[Bookings]
