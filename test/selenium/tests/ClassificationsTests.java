64f3d33d6413a54405472db072dbe76b6e2eeb93
RGlossop <RGlossop@qa.com>
2021-02-25 11:41:07 +0000

    QCTA-114 RG UserDAO integration tests and also changed path of chrome driver

--- test/selenium/tests/ClassificationsTests.java
+++ test/selenium/tests/ClassificationsTests.java
@@ -21,7 +21,7 @@ public class ClassificationsTests {
 
     @BeforeClass
     public static void init() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/Ryan/Desktop/QACinemas/test/resources/drivers/chrome/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "test/resources/drivers/chrome/chromedriver.exe");
     }
     @Before
     public void setup() {
